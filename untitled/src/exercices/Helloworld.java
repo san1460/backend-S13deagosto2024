2c1e8fec162c819e7757ff4499b2a155fb5966cd
san1460 <165798237+san1460@users.noreply.github.com>
2024-08-13 07:20:58 -0500

    con mi inicial
    se crea clase 13 de agosto

---
+++ untitled/src/exercices/Helloworld.java
@@ -0,0 +1,46 @@
+package exercices;
+
+public class Helloworld {
+
+ public static void  main(String [] args){
+
+     System.out.println("hola mundo mi nombre es santiago");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
